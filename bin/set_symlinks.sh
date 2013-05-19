d2b3482ae7cd7cbd1db59ebbb88adf7bc33bfd0a
key-amb <key-amb@github.com>
2013-05-19 02:11:24 +0000

    set symlinks to homedir

---
+++ bin/set_symlinks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROJECT=dotfiles
+DOTFILES=(.zshenv .zshrc .vimrc)
+CMD=""
+
+cd $HOME
+if [ -e $PROJECT ]; then
+  printf "${HOME}/$PROJECT exists\n"
+else
+  ln -s gitrepos/${PROJECT}
+fi
+
+for file in ${DOTFILES[@]}; do
+  if [ -e $file ]; then
+    printf "${HOME}/$file exists\n"
+  else
+    ln -s $PROJECT/$file
+  fi
+done
+
