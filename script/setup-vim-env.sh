b2dd8787f01da1813407aee60c6db2393e865ce0
progrhyme <progrhyme@gmail.com>
2020-05-09 14:00:03 +0900

    bug fix

--- script/setup-vim-env.sh
+++ script/setup-vim-env.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
+############################################################
+# Initialize
+
 PROJECT=dotfiles
 VIM_DIR=${HOME}/.vim
 TARGETS=(.vimrc .vim/ftplugin)
+
+############################################################
+# Functions
+
+symlink() {
+  file=$1;
+  link="${HOME}/$file"
+  real_file="${PROJECT}/${file}";
+
+  if [ -L $link ]; then
+    echo "[ok] already linked: ${link} -> ${real_file}";
+    return 0;
+  elif [ -d $link ]; then
+    echo "[warn] directory already exists: $link";
+    return 0;
+  elif [ -f $link ]; then
+    echo "[warn] file already exists: $link";
+    return 0;
+  fi
+
+  if [ -e $real_file ]; then
+    ln -s $real_file $link;
+    echo "[ok] linked: $link -> ${real_file}";
+  else
+    echo "[error] not exist: ${real_file}";
+    return 1;
+  fi
+
+  return 0;
+}
+
+############################################################
+# Main
 
 cd $HOME
 if [ -e $PROJECT ]; then
@@ -18,10 +54,6 @@ fi
 
 # create symlinks
 for t in ${TARGETS[@]}; do
-  if [ -e $t ]; then
-    printf "${HOME}/$t exists\n"
-  else
-    ln -s $PROJECT/$t
-  fi
+  symlink $t
 done
 
